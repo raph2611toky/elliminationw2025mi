1d26ab4abf1c4aff99b4cfa55ed25c8d53437831
raph2611toky <raphaeltokinandrasana@gmail.com>
2025-05-18 03:35:45 +0300

    lady

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
-import Landingpage from './landinpage/landingpage'
-import Inscription from './authentification/inscription'
-import Connexion from './authentification/connexion'
-import { languageContext, darkModeContext } from './hooks/context'
+import Landingpage from './component/landingpage'
+import HallDesFins from './hallDesFins/hallDesFins'
 function App() {
   
   return(<>
    <Routes>
-            <Route path ="/"  element={<Landingpage/>}/>
-            <Route path ="/inscription"  element={<Inscription/>}/>
-            <Route path='/connexion' element={<Connexion/>}/>
+      <Route path ="/"  element={<Landingpage/>}/>
+      <Route path ="/hall"  element={<HallDesFins/>}/>
+            
    </Routes>  
   </>)
 }
---
+++ frontend/src/apresFin/apresFin.jsx
@@ -0,0 +1,12 @@
+function ApresFin(){
+
+    return(
+        <div>
+
+
+        </div>
+    );
+
+
+}
+export default ApresFin
